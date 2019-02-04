93ba3fa80abfea182237d7c790bfb5448e70bf0f
TuyetGiang <30744004+TuyetGiang@users.noreply.github.com>
2019-02-05 01:58:03 +0700

    [GiangPTT][web_admin_api]user_role_benefit_api
    this is first commit for web admin api

---
+++ src/main/java/com/giang/config/SwaggerConfig.java
@@ -0,0 +1,11 @@
+package com.giang.config;
+
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+
+
+public class SwaggerConfig {
+
+
+
+}
